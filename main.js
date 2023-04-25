b0a93a4c90a48f2012c1615e11f94b16e3539bda
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-04-25 21:50:01 +0100

    javascript update

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const unreadQnty = document.querySelector(".quantity");
 const markRead = document.querySelector(".mark-read");
-const notification = document.querySelector(".notification");
+const notifications = document.querySelectorAll(".notification");
 
 markRead.addEventListener("click", () => {
   unreadQnty.innerHTML = 0;
-  notification.hasAttribute("data-type")
-    ? notification.toggleAttribute("data-type")
-    : console.log("all messages are read");
+  notifications.forEach((notification) => {
+    notification.hasAttribute("aria-live", true)
+      ? notification.setAttribute("aria-live", false)
+      : console.log("all messages are read");
+  });
 });
 
-notification.addEventListener("click", () => {
-  notification.toggleAttribute("data-type");
-  // ? notification.toggleAttribute("data-type")
-  // : console.log("all messages are read");
+notifications.forEach((notification) => {
+  notification.addEventListener("click", () => {
+    notification.setAttribute("aria-live", false);
+    unreadQnty.innerHTML = unreadQnty.innerHTML - 1;
+  });
 });
