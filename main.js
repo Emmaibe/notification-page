b742f4405e60bc7bff16ee1a6dc9171a161eac9e
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-04-26 18:20:03 +0100

    js tweeking

--- main.js
+++ main.js
@@ -3,17 +3,16 @@ const markRead = document.querySelector(".mark-read");
 const notifications = document.querySelectorAll(".notification");
 
 markRead.addEventListener("click", () => {
-  unreadQnty.innerHTML = 0;
+  unreadQnty.innerText = 0;
   notifications.forEach((notification) => {
-    notification.hasAttribute("aria-live", true)
-      ? notification.setAttribute("aria-live", false)
-      : console.log("all messages are read");
+    notification.classList.remove("unread");
   });
 });
 
 notifications.forEach((notification) => {
   notification.addEventListener("click", () => {
-    notification.setAttribute("aria-live", false);
-    unreadQnty.innerHTML = unreadQnty.innerHTML - 1;
+    notification.classList.remove("unread");
+    const newQnty = document.querySelectorAll(".unread");
+    unreadQnty.innerText = newQnty.length;
   });
 });
